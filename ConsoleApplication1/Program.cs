86ea32b3d9f1179eb578b93d1bb7889fb8f3e6f1
ben <ben@matrixacoustics.com.au>
2017-12-14 10:45:14 +1000

    initial coding with Geoff

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,149 @@
+﻿using Matrix.Sensors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Timers;
+
+namespace Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = new Logger();
+            while (true) { }
+        }
+    }
+
+    // ACCESSIBILITY
+    // public = anything with the dll can access it
+    // internal = only stuff within the dll can access it
+    // protected = only stuff that inherits form this class can access it
+    // private = only this class can access it
+    public class Site
+    {
+        // general information about the site
+        public FrequencyEnum Frequency;
+    }
+
+    // Static
+    // Static stuff exists once in memory no matter how many objects are created
+    // Static stuff can be accessed without 'instantiating' up the class
+
+
+
+    public class Logger
+    {
+        // The site this logger is part of
+        public Site Site;
+
+        // The sensors in this logger
+        public DavisWeatherSensor DavisWeatherSensor;
+        public FineOffsetWeatherSensor FineOffsetWeatherSensor;
+        public XL2 XL2;
+
+        // a sensors collection
+        public List<Sensor> Sensors;
+
+        // dispatches events to schedule
+        public Timer Timer;
+
+        public Logger()
+        {
+            // New up the sensors. This could be based on configuration e.g.
+            // if (ConfigurationManager.AppSettings["HasDavisWeatherSensor"] == true) 
+            DavisWeatherSensor = new DavisWeatherSensor();
+            FineOffsetWeatherSensor = new FineOffsetWeatherSensor();
+            XL2 = new XL2(
+                    metrics: new List<XL2Metric>
+                    {
+                        new LAEQ(),
+                        new LAFMAX()
+                    },
+                    range: "MID"        
+                );
+
+            Sensors = new List<Sensor>()
+            {
+                DavisWeatherSensor,
+                FineOffsetWeatherSensor,
+                XL2,
+            };
+
+            Timer = new Timer(5 * 1000);
+            Timer.Elapsed += OnTimer;
+            Timer.AutoReset = true;
+            Timer.Enabled = true;
+        }
+
+        public void OnTimer(Object source, ElapsedEventArgs eventArgs)
+        {
+            Console.WriteLine("Timer event triggered...");
+
+            try
+            {
+                Sensors.ForEach(x => x.CompleteMeasurementPeriod());
+                Sensors.ForEach(x => x.CommenceMeasurementPeriod());
+
+                foreach (var s in Sensors)
+                {
+                    var measurement = s.ReadMeasuredValue();
+
+                    if (measurement is XL2Reading)
+                    {
+                        var xl2Reading = measurement as XL2Reading;
+
+                        // post to Simon :)
+                        xl2Reading.MetricReadings.ForEach(x => Console.WriteLine($"XL2's {x.Metric.GetType().Name} Measurment is {x.Measurement}"));
+                    }
+
+                    if (measurement is FineOffsetWeatherReading)
+                    {
+
+                    }
+
+                    if (measurement is ErrorReadingSensor)
+                    {
+                        var error = measurement as ErrorReadingSensor;
+                        Console.WriteLine($"Error reading sensor {s.GetType().Name}");
+                        if (error.CommenceException != null)
+                        {
+                            Console.WriteLine($"CommenceException {error.CommenceException.Message}");
+                        }
+                        if (error.CompleteException != null)
+                        {
+                            Console.WriteLine($"CompleteException {error.CompleteException.Message}");
+                        }
+                        if (error.ReadException != null)
+                        {
+                            Console.WriteLine($"ReadException {error.ReadException.Message}");
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                // decide to try something here.
+            }
+            
+        }
+
+    }
+
+    public class Scheduler
+    {
+
+    }
+
+    public enum FrequencyEnum
+    {
+        FIFTEEN_MINUTES,
+        ONE_HOUR
+    }
+
+
+
+}
---
+++ ConsoleApplication1/Sensor/FineOffsetWeatherSensor.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix.Sensors
+{
+    
+    public class FineOffsetWeatherSensor : WeatherSensor
+    {
+        protected override SensorReading ReadValues()
+        {
+            return new FineOffsetWeatherReading() { };
+        }
+    }
+
+    public class FineOffsetWeatherReading : SensorReading
+    { }
+
+
+    // abstract = must be overwritten in a child class
+    // virtual = can be overwritten in a child class
+
+    public abstract class Sensor
+    {
+        private Exception CommenceException = null;
+        private Exception CompleteException = null;
+        private Exception ReadException = null;
+
+        public void CommenceMeasurementPeriod()
+        {
+            try
+            {
+                CommenceMeasurement();
+                CommenceException = null;
+            }
+            catch (Exception e)
+            {
+                CommenceException = e;
+            }
+        }
+
+        public void CompleteMeasurementPeriod()
+        {
+            try
+            {
+                CompleteMeasurement();
+                CompleteException = null;
+            }
+            catch (Exception e)
+            {
+                CompleteException = e;
+            }
+        }
+
+        public SensorReading ReadMeasuredValue()
+        {
+
+            SensorReading reading = null;
+            try
+            {
+                reading = ReadValues();
+                ReadException = null;
+            }
+            catch (Exception e)
+            {
+                ReadException = e;
+            }
+
+            if (CommenceException != null || CompleteException != null || ReadException != null)
+            {
+                return new ErrorReadingSensor
+                {
+                    CommenceException = CommenceException,
+                    CompleteException = CompleteException,
+                    ReadException = ReadException,
+                    Data = reading
+                };
+            }
+
+            return reading;
+        }
+
+        protected virtual void CommenceMeasurement() { }
+        protected virtual void CompleteMeasurement() { }
+        protected abstract SensorReading ReadValues();
+    }
+
+   
+
+    public abstract class WeatherSensor : Sensor
+    {
+
+    }
+
+    public class DavisWeatherSensor : WeatherSensor
+    {
+        protected override SensorReading ReadValues()
+        {
+            return new FineOffsetWeatherReading() { };
+
+        }
+    }
+
+    public class SensorReading
+    {
+
+    }
+
+
+    public class DavisWeatherSensorReading : SensorReading
+    {
+
+    }
+
+    public class XL2 : Sensor
+    {
+        private XL2Reading _lastReading;
+        private SerialPort ComPort;
+        private List<XL2Metric> MetricsToRead;
+
+        public XL2(List<XL2Metric> metrics, string range)
+        {
+            MetricsToRead = metrics;
+            ComPort = new SerialPort("COM4");
+            ComPort.Open();
+            //ComPort.Write($"INPUT:RANGE {ConfigurationManager.AppSettings["XL2Range"]} \n");
+            ComPort.Write($"INPUT:RANGE {range} \n");
+        }
+
+        protected override void CommenceMeasurement()
+        {
+            // Start
+            if (!ComPort.IsOpen)
+            {
+                ComPort.Open();
+            }
+            ComPort.Write("INIT START\n");
+        }
+
+        protected override void CompleteMeasurement()
+        {
+            // Stop
+            ComPort.Write("INIT STOP\n");
+
+            // Get ready to collect measurments
+            ComPort.Write("MEAS:INIT\n");
+        }
+
+        protected override SensorReading ReadValues()
+        {
+            // Read
+            return new XL2Reading
+            {
+                MetricReadings = MetricsToRead.Select(
+                    x =>
+                        new XL2MetricReading
+                        {
+                            Metric = x,
+                            Measurement = GetMeasurement(x)
+                        }
+                ).ToList()
+            };
+        }
+
+        private string GetMeasurement(XL2Metric metric)
+        {
+            ComPort.Write(metric.Command);
+            return ComPort.ReadLine();
+        }
+    }
+
+    public abstract class XL2Metric
+    {
+        public abstract string Command { get; }
+    }
+
+    public class LAEQ : XL2Metric
+    {
+        public override string Command => "MEAS:SLM:123? LAEQ\n";
+    }
+
+    public class LAFMAX : XL2Metric
+    {
+        public override string Command => "MEAS:SLM:123? LAFMAX\n";
+
+    }
+
+    public class XL2Reading : SensorReading
+    {
+        public List<XL2MetricReading> MetricReadings;
+    }
+
+    public class ErrorReadingSensor : SensorReading
+    {
+        public Exception CommenceException;
+        public Exception CompleteException;
+        public Exception ReadException;
+        public SensorReading Data;
+    }
+
+    public class XL2MetricReading
+    {
+        public XL2Metric Metric;
+        public string Measurement;
+    }
+}
